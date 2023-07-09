eeffa0ce3753b6b0c629d1ff51852cd362bc58e5
tiagodacosta1 <tiagocosta4823@gmail.com>
2023-07-09 17:01:01 +0100

    Initial commit

---
+++ assets/database.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyByQppD28GBCJHgTexra4uHMND8LAXsAEY",
+    authDomain: "cam-wars.firebaseapp.com",
+    databaseURL: "https://cam-wars.firebaseio.com",
+    projectId: "cam-wars",
+    storageBucket: "cam-wars.appspot.com",
+    messagingSenderId: "7132401827",
+    appId: "1:7132401827:web:657896d748ee6fc8acaf16"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
